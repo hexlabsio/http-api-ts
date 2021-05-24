4cfaff1955a2ee9125fd3feac47ba707325b4b08
Chris Barbour <chris.barbour03@gmail.com>
2021-05-24 14:29:20 +0100

    adding invoker

--- src/invoker.ts
+++ src/invoker.ts
@@ -1,27 +1,36 @@
-import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
+import {APIGatewayProxyResult} from "aws-lambda";
 import {Lambda} from "aws-sdk";
+import axios from 'axios';
 
-export type PathParams<T extends string> =
-  T extends `${infer Start}{${infer PathParam}}/${infer Rest}`
-    ? { [k in PathParam | keyof PathParams<Rest>]: string }
-    : T extends `${infer Start}{${infer PathParam}}`
-    ? {[k in PathParam]: string}
-    : undefined;
-
-export type InvokeParams<R extends string> = Partial<APIGatewayProxyEvent> & {
-  resource: R,
-  pathParameters: PathParams<R>
+type Caller = {
+  call(
+    method: string,
+    resource: string,
+    path: string,
+    body: string | undefined,
+    pathParameters: Record<string, string>,
+    queryParameters: Record<string, string>,
+    headers: Record<string, string>
+  ): Promise<{ statusCode: number; body: string; headers: Record<string, string> }>
 }
 
 export class Invoker {
-  constructor(private readonly lambda = new Lambda()) {}
   
-  async invoke<R extends string>(arn: string, params: InvokeParams<R>): Promise<APIGatewayProxyResult> {
-    const result = await this.lambda.invoke({FunctionName: arn, InvocationType: 'RequestResponse', Payload: JSON.stringify(params)}).promise();
-    if(result.StatusCode! >= 200 && result.StatusCode! < 500) {
-      return JSON.parse(result.Payload!.toString());
-    }
-    throw new Error(result.FunctionError ?? 'Unknown Invoke Error');
+  static lambdaInvoker(arn: string, lambda = new Lambda()): Caller {
+    return { call: (async (method, resource, path, body, pathParameters, queryParameters, headers) => {
+        const result = await lambda.invoke({FunctionName: arn, InvocationType: 'RequestResponse', Payload: JSON.stringify({ httpMethod: method, resource, path, body: body ?? null, pathParameters: pathParameters as any, headers, queryStringParameters: queryParameters})}).promise();
+        if(result.StatusCode! >= 200 && result.StatusCode! < 500) {
+          const response : APIGatewayProxyResult = JSON.parse(result.Payload!.toString());
+          return {statusCode: response.statusCode, body: response.body, headers: response.headers as any };
+        }
+        throw new Error(result.FunctionError ?? 'Unknown Invoke Error');
+      }) };
   }
   
+  static httpInvoker(uri: string): Caller {
+    return { call: (async (method, resource, path, body, pathParameters, queryParameters, headers) => {
+        const result = await axios(uri + path, {method: method as any, data: body, params: pathParameters, headers});
+        return {statusCode: result.status, body: result.data, headers: result.headers as any};
+      }) };
+  }
 }
