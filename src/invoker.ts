0c80fd10d29bff7ec196bc06040f814dc7b3a6a0
Chris Barbour <chris.barbour03@gmail.com>
2021-05-26 13:23:00 +0100

    updatinbg caller signature

--- src/invoker.ts
+++ src/invoker.ts
@@ -1,4 +1,4 @@
-import {APIGatewayProxyResult} from "aws-lambda";
+import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
 import {Lambda} from "aws-sdk";
 import axios from 'axios';
 
@@ -10,6 +10,7 @@ type Caller = {
     body: string | undefined,
     pathParameters: Record<string, string>,
     queryParameters: Record<string, string>,
+    multiQueryParameters: Record<string, string[]>,
     headers: Record<string, string>
   ): Promise<{ statusCode: number; body: string; headers: Record<string, string> }>
 }
@@ -17,8 +18,9 @@ type Caller = {
 export class Invoker {
   
   static lambdaInvoker(arn: string, lambda = new Lambda()): Caller {
-    return { call: (async (method, resource, path, body, pathParameters, queryParameters, headers) => {
-        const result = await lambda.invoke({FunctionName: arn, InvocationType: 'RequestResponse', Payload: JSON.stringify({ httpMethod: method, resource, path, body: body ?? null, pathParameters: pathParameters as any, headers, queryStringParameters: queryParameters})}).promise();
+    return { call: (async (method, resource, path, body, pathParameters, queryParameters, multiQueryParameters, headers) => {
+        const event = { httpMethod: method, resource, path, body: body ?? null, pathParameters: pathParameters as any, multiValueQueryStringParameters: multiQueryParameters, headers, queryStringParameters: queryParameters} as APIGatewayProxyEvent;
+        const result = await lambda.invoke({FunctionName: arn, InvocationType: 'RequestResponse', Payload: JSON.stringify(event)}).promise();
         if(result.StatusCode! >= 200 && result.StatusCode! < 500) {
           const response : APIGatewayProxyResult = JSON.parse(result.Payload!.toString());
           return {statusCode: response.statusCode, body: response.body, headers: response.headers as any };
@@ -28,8 +30,8 @@ export class Invoker {
   }
   
   static httpInvoker(uri: string): Caller {
-    return { call: (async (method, resource, path, body, pathParameters, queryParameters, headers) => {
-        const result = await axios(uri + path, {method: method as any, data: body, params: pathParameters, headers, transformResponse: []});
+    return { call: (async (method, resource, path, body, pathParameters, queryParameters, multiQueryParameters, headers) => {
+        const result = await axios(uri + path, {method: method as any, data: body, params: { ...queryParameters, ...multiQueryParameters} , headers, transformResponse: []});
         return {statusCode: result.status, body: result.data, headers: result.headers as any};
       }) };
   }
